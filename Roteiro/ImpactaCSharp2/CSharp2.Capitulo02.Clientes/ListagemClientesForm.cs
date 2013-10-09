0ad082f2796b417fda6b3a22fc1e5dd6d3308c1a
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-10-09 21:01:40 +0000

    Pesquisar Clientes.

---
+++ Roteiro/ImpactaCSharp2/CSharp2.Capitulo02.Clientes/ListagemClientesForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using Impacta.Infra.Repositorios.SqlServer;
+
+namespace CSharp2.Capitulo02.Clientes
+{
+    public partial class ListagemClientesForm : Form
+    {
+        public ListagemClientesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ListagemClientesForm_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = nomePesquisadoToolStripTextBox.Control;
+            clientesDataGridView.AutoGenerateColumns = false;
+        }
+
+        private void pesquisarToolStripButton_Click(object sender, EventArgs e)
+        {
+            Pesquisar();
+        }
+
+        private void Pesquisar()
+        {
+            if (nomePesquisadoToolStripTextBox.Text.Trim() == string.Empty)
+            {
+                clientesDataGridView.DataSource = null;
+                return;
+            }
+
+            clientesDataGridView.DataSource = new ClienteRepositorio().Selecionar(nomePesquisadoToolStripTextBox.Text);
+        }
+
+        private void nomePesquisadoToolStripTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Pesquisar();
+            }
+        }
+    }
+}
---
+++ Roteiro/ImpactaCSharp2/Impacta.Infra.Repositorios.SqlServer/ClienteRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Impacta.Infra.Repositorios.SqlServer
+{
+    public class ClienteRepositorio
+    {
+        public DataTable Selecionar(string nomeCliente)
+        {
+            var conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["oficinaConnectionString"].ConnectionString);
+            conexao.Open();
+
+            const string instrucao = @"SELECT [Id]
+                      ,[Nome]
+                      ,[Email]
+                      ,[DataNascimento]
+                  FROM [Oficina].[dbo].[Cliente]
+                  Where Nome like '%' + @nome + '%'";
+
+            var comando = new SqlCommand(instrucao, conexao);
+            comando.Parameters.AddWithValue("@nome", nomeCliente);
+
+            var dataSet = new DataSet();
+
+            var dataAdapter = new SqlDataAdapter(comando);
+            dataAdapter.Fill(dataSet);
+
+            conexao.Close();
+
+            return dataSet.Tables[0];
+        }
+    }
+}
