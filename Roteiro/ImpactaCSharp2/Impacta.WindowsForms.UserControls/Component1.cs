71dd44500ddeaa1c1d1d80e547915dc712539ab2
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-10-22 00:58:06 +0000

    Componentes.

--- Roteiro/ImpactaCSharp2/Impacta.WindowsForms.UserControls/Component1.cs
+++ Roteiro/ImpactaCSharp2/Impacta.WindowsForms.UserControls/Component1.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel;
 
-namespace Impacta.WindowsForms.Controles
+namespace Impacta.Apoio.WindowsForms.Controles
 {
     public partial class Component1 : Component
     {
