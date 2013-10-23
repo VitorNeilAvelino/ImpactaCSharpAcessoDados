966798f3e9371fe702ced2cbad6e76f2377b027b
vitoravelino_cp <SND\vitoravelino_cp>
2013-10-23 14:50:29 +0000

    Indexadores.

---
+++ Roteiro/ImpactaCSharp2/Impacta.Dominio.Testes/VeiculoTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Impacta.Dominio.Testes
+{
+    [TestClass]
+    public class VeiculoTest
+    {
+        [TestMethod]
+        public void IndexadorVeiculoTeste()
+        {
+            var veiculos = new List<Veiculo>();
+            veiculos.Add(new Veiculo { Id = 1, Placa = "ETH6834" });
+            veiculos.Add(new Veiculo { Id = 2, Placa = "ETH6835" });
+
+            foreach (var veiculo in veiculos)
+            {
+                if (veiculo["ETH6834"] != null)
+                {
+                    Console.WriteLine(veiculo.Id);
+                }
+            }
+
+            var veiculoLambda = veiculos.FirstOrDefault(v => v.Placa == "ETH6834");
+            if (veiculoLambda != null)
+            {
+                Console.WriteLine(veiculoLambda.Id);
+            }
+        }
+    }
+}
