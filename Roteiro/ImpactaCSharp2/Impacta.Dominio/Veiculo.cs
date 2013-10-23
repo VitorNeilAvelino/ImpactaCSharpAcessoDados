3aae11b644138d26f8929ab00113539e387e0694
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-10-23 14:50:29 +0000

    Indexadores.

---
+++ Roteiro/ImpactaCSharp2/Impacta.Dominio/Veiculo.cs
@@ -0,0 +1,15 @@
+﻿namespace Impacta.Dominio
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string Placa { get; set; }
+        public int AnoFabricacao { get; set; }
+        public int AnoModelo { get; set; }
+
+        public Veiculo this[string placa]
+        {
+            get { return placa == Placa ? this : null; }
+        }
+    }
+}
