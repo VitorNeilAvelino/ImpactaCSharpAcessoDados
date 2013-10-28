cbd17fd8307a7e5c55a7b8ea9e415fca05928c26
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-10-28 18:50:35 +0000

    WPF.

--- Roteiro/ImpactaCSharp2/CSharp2.Capitulo08.Wpf/App.xaml.cs
+++ Roteiro/ImpactaCSharp2/CSharp2.Capitulo08.Wpf/App.xaml.cs
@@ -4,6 +4,8 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Threading;
+using System.Globalization;
 
 namespace CSharp2.Capitulo08.Wpf
 {
@@ -12,5 +14,12 @@ namespace CSharp2.Capitulo08.Wpf
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("pt-BR");
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("pt-BR");
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(
+            System.Windows.Markup.XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+        }
     }
 }
