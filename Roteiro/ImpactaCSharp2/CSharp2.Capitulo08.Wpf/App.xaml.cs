a63d974f73dd28c84767d9d53bf580647d0a3d6e
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-10-31 21:10:02 +0000

    WPF e Threads.

--- Roteiro/ImpactaCSharp2/CSharp2.Capitulo08.Wpf/App.xaml.cs
+++ Roteiro/ImpactaCSharp2/CSharp2.Capitulo08.Wpf/App.xaml.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 using System.Threading;
 using System.Globalization;
 
 namespace CSharp2.Capitulo08.Wpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
         public App()
         {
-            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("pt-BR");
-            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("pt-BR");
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("pt-BR");
+            
             FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(
             System.Windows.Markup.XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
         }
