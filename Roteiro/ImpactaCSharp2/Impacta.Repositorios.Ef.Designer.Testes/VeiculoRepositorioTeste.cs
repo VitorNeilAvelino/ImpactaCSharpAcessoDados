0b9958ec0021e38f60399250bb00815c4e6b4c8f
vitoravelino_cp <SND\vitoravelino_cp>
2014-05-23 01:57:32 +0000

    Testes com EF.Designer

---
+++ Roteiro/ImpactaCSharp2/Impacta.Repositorios.Ef.Designer.Testes/VeiculoRepositorioTeste.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+namespace Impacta.Repositorios.Ef.Designer.Testes
+{
+    [TestClass]
+    public class VeiculoRepositorioTeste
+    {
+        [TestMethod]
+        public void InserirTeste()
+        {
+            var db = new OficinaEntities();
+
+            using (OficinaEntities contexto = new OficinaEntities())
+            {
+                var veiculo = new Veiculo();
+                veiculo.AnoFabricacao = 2013;
+                veiculo.AnoModelo = 2014;
+                veiculo.Cor = contexto.Cor.Single(c => c.Id == 1);
+                veiculo.Modelo = contexto.Modelo.Single(m => m.Id == 1);
+                veiculo.Placa = "FIB6967";
+
+                contexto.Veiculo.Add(veiculo);
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
