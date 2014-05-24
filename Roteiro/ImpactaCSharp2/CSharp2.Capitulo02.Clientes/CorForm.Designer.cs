eda1577fb94182172a1a0b0371df01191abb273b
vitoravelino_cp <SND\vitoravelino_cp>
2014-05-24 15:42:45 +0000

    Datasource e RAD para Cor e Modelo.

---
+++ Roteiro/ImpactaCSharp2/CSharp2.Capitulo02.Clientes/CorForm.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace CSharp2.Capitulo02.Clientes
+{
+    partial class CorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CorForm));
+            this.oficinaDataSet = new CSharp2.Capitulo02.Clientes.OficinaDataSet();
+            this.corBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.corTableAdapter = new CSharp2.Capitulo02.Clientes.OficinaDataSetTableAdapters.CorTableAdapter();
+            this.tableAdapterManager = new CSharp2.Capitulo02.Clientes.OficinaDataSetTableAdapters.TableAdapterManager();
+            this.corBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.corBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.corDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.oficinaDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.corBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.corBindingNavigator)).BeginInit();
+            this.corBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.corDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // oficinaDataSet
+            // 
+            this.oficinaDataSet.DataSetName = "OficinaDataSet";
+            this.oficinaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // corBindingSource
+            // 
+            this.corBindingSource.DataMember = "Cor";
+            this.corBindingSource.DataSource = this.oficinaDataSet;
+            // 
+            // corTableAdapter
+            // 
+            this.corTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.ClienteTableAdapter = null;
+            this.tableAdapterManager.ClienteVeiculoTableAdapter = null;
+            this.tableAdapterManager.CorTableAdapter = this.corTableAdapter;
+            this.tableAdapterManager.ModeloTableAdapter = null;
+            this.tableAdapterManager.ServicoTableAdapter = null;
+            this.tableAdapterManager.TipoServicoTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = CSharp2.Capitulo02.Clientes.OficinaDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.VeiculoTableAdapter = null;
+            // 
+            // corBindingNavigator
+            // 
+            this.corBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.corBindingNavigator.BindingSource = this.corBindingSource;
+            this.corBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.corBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.corBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.corBindingNavigatorSaveItem});
+            this.corBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.corBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.corBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.corBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.corBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.corBindingNavigator.Name = "corBindingNavigator";
+            this.corBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.corBindingNavigator.Size = new System.Drawing.Size(468, 25);
+            this.corBindingNavigator.TabIndex = 0;
+            this.corBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 15);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // corBindingNavigatorSaveItem
+            // 
+            this.corBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.corBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("corBindingNavigatorSaveItem.Image")));
+            this.corBindingNavigatorSaveItem.Name = "corBindingNavigatorSaveItem";
+            this.corBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.corBindingNavigatorSaveItem.Text = "Save Data";
+            this.corBindingNavigatorSaveItem.Click += new System.EventHandler(this.corBindingNavigatorSaveItem_Click);
+            // 
+            // corDataGridView
+            // 
+            this.corDataGridView.AutoGenerateColumns = false;
+            this.corDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.corDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.corDataGridView.DataSource = this.corBindingSource;
+            this.corDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.corDataGridView.Location = new System.Drawing.Point(0, 25);
+            this.corDataGridView.Name = "corDataGridView";
+            this.corDataGridView.Size = new System.Drawing.Size(468, 375);
+            this.corDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Descricao";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Descricao";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // CorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 400);
+            this.Controls.Add(this.corDataGridView);
+            this.Controls.Add(this.corBindingNavigator);
+            this.Name = "CorForm";
+            this.Text = "CorForm";
+            this.Load += new System.EventHandler(this.CorForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.oficinaDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.corBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.corBindingNavigator)).EndInit();
+            this.corBindingNavigator.ResumeLayout(false);
+            this.corBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.corDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OficinaDataSet oficinaDataSet;
+        private System.Windows.Forms.BindingSource corBindingSource;
+        private OficinaDataSetTableAdapters.CorTableAdapter corTableAdapter;
+        private OficinaDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator corBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton corBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView corDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+    }
+}
---
+++ Roteiro/ImpactaCSharp2/CSharp2.Capitulo02.Clientes/CorForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSharp2.Capitulo02.Clientes
+{
+    public partial class CorForm : Form
+    {
+        public CorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void corBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.corBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.oficinaDataSet);
+
+        }
+
+        private void CorForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'oficinaDataSet.Cor' table. You can move, or remove it, as needed.
+            this.corTableAdapter.Fill(this.oficinaDataSet.Cor);
+
+        }
+    }
+}
