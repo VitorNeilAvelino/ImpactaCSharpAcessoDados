34d09bbaacfdd04393d112462b2776bdcf61f7a6
vitoravelino_cp <SND\vitoravelino_cp>
2014-05-26 00:54:55 +0000

    Testes com Select.

--- Roteiro/ImpactaCSharp2/Impacta.Repositorios.Ef.Designer.Testes/ModeloRepositorioTeste.cs
+++ Roteiro/ImpactaCSharp2/Impacta.Repositorios.Ef.Designer.Testes/ModeloRepositorioTeste.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 
 namespace Impacta.Repositorios.Ef.Designer.Testes
@@ -16,15 +15,18 @@ namespace Impacta.Repositorios.Ef.Designer.Testes
                               where m.Descricao == "C4"
                               select m;
 
-                var montadora = new Montadora { Nome = "Citroën" };
+                var montadora = new Montadora { Nome = "Ford" };
 
-                modelos.ToList().ForEach(m => m.Montadora = montadora);
+                foreach (var modelo in modelos)
+                {
+                    modelo.Montadora = montadora;
+                }
 
-                //foreach (var modelo in modelos)
-                //{
-                //    modelo.Montadora = montadora;
-                //}
+                //db.Modelo.Where(m => m.Descricao == "Palio").ToList().ForEach(m => m.Montadora = new Montadora { Nome = "Fiat" });
 
+                //var coisa = db.Veiculo.Where(v => v.Placa == "ETH6834").Select(v => new { v.Placa, v.AnoFabricacao }).Single();
+                //Assert.AreEqual(coisa.Placa, "ETH6834");
+                
                 db.SaveChanges();
             }
         }
