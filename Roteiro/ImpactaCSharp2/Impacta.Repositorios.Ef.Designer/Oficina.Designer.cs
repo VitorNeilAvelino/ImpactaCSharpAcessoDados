76030ecc7dcc15f96f3ec5a3186c60acb34cf45a
vitoravelino_cp <SND\vitoravelino_cp>
2014-05-28 00:58:45 +0000

    LinqTeste.

--- Roteiro/ImpactaCSharp2/Impacta.Repositorios.Ef.Designer/Oficina.Designer.cs
+++ Roteiro/ImpactaCSharp2/Impacta.Repositorios.Ef.Designer/Oficina.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Vítor\Documents\Visual Studio 2010\Projects\impactacsharp2\Roteiro\ImpactaCSharp2\Impacta.Repositorios.Ef.Designer\Oficina.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Projetos\Impacta\CSharp2\Roteiro\ImpactaCSharp2\Impacta.Repositorios.Ef.Designer\Oficina.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
