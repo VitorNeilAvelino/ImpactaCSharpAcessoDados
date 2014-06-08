7999a6d37d7341ba4a23368c50b69d4520c64d61
vitoravelino_cp <SND\vitoravelino_cp>
2014-06-08 05:13:38 +0000

    WPF.

--- Roteiro/ImpactaCSharp2/CSharp2.Capitulo10.Wpf/App.xaml.cs
+++ Roteiro/ImpactaCSharp2/CSharp2.Capitulo10.Wpf/App.xaml.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Markup;
 
 namespace CSharp2.Capitulo10.Wpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            //Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("pt-BR");
+
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(XmlLanguage.GetLanguage("pt-BR")));
+        }
     }
 }
