6e8cf35640138c8a50d90d64f52063da1358ef83
vitoravelino_cp <SND\vitoravelino_cp>
2014-10-10 02:19:46 +0000

    ProdutoForm.

---
+++ Roteiro/ImpactaCSharp2/CSharp2.Capitulo02.Produtos/ProdutoForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace CSharp2.Capitulo02.Produtos
+{
+    partial class ProdutoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.descricaoTextBox = new System.Windows.Forms.TextBox();
+            this.custoTextBox = new System.Windows.Forms.TextBox();
+            this.tipoComboBox = new System.Windows.Forms.ComboBox();
+            this.gravarButton = new System.Windows.Forms.Button();
+            this.produtoErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.produtoErrorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&Descrição";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "&Tipo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "&Custo";
+            // 
+            // descricaoTextBox
+            // 
+            this.descricaoTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.descricaoTextBox.Location = new System.Drawing.Point(73, 6);
+            this.descricaoTextBox.Name = "descricaoTextBox";
+            this.descricaoTextBox.Size = new System.Drawing.Size(399, 20);
+            this.descricaoTextBox.TabIndex = 1;
+            this.descricaoTextBox.Tag = "*";
+            // 
+            // custoTextBox
+            // 
+            this.custoTextBox.Location = new System.Drawing.Point(73, 98);
+            this.custoTextBox.Name = "custoTextBox";
+            this.custoTextBox.Size = new System.Drawing.Size(121, 20);
+            this.custoTextBox.TabIndex = 5;
+            this.custoTextBox.Tag = "*DECIMAL";
+            // 
+            // tipoComboBox
+            // 
+            this.tipoComboBox.FormattingEnabled = true;
+            this.tipoComboBox.Location = new System.Drawing.Point(73, 52);
+            this.tipoComboBox.Name = "tipoComboBox";
+            this.tipoComboBox.Size = new System.Drawing.Size(121, 21);
+            this.tipoComboBox.TabIndex = 3;
+            this.tipoComboBox.Tag = "*";
+            // 
+            // gravarButton
+            // 
+            this.gravarButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.gravarButton.Location = new System.Drawing.Point(417, 295);
+            this.gravarButton.Name = "gravarButton";
+            this.gravarButton.Size = new System.Drawing.Size(75, 23);
+            this.gravarButton.TabIndex = 6;
+            this.gravarButton.Text = "&Gravar";
+            this.gravarButton.UseVisualStyleBackColor = true;
+            this.gravarButton.Click += new System.EventHandler(this.gravarButton_Click);
+            // 
+            // produtoErrorProvider
+            // 
+            this.produtoErrorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.produtoErrorProvider.ContainerControl = this;
+            // 
+            // ProdutoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 330);
+            this.Controls.Add(this.gravarButton);
+            this.Controls.Add(this.tipoComboBox);
+            this.Controls.Add(this.custoTextBox);
+            this.Controls.Add(this.descricaoTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ProdutoForm";
+            this.Text = "Produto";
+            ((System.ComponentModel.ISupportInitialize)(this.produtoErrorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox descricaoTextBox;
+        private System.Windows.Forms.TextBox custoTextBox;
+        private System.Windows.Forms.ComboBox tipoComboBox;
+        private System.Windows.Forms.Button gravarButton;
+        private System.Windows.Forms.ErrorProvider produtoErrorProvider;
+    }
+}
+
---
+++ Roteiro/ImpactaCSharp2/CSharp2.Capitulo02.Produtos/ProdutoForm.cs
@@ -0,0 +1,58 @@
+﻿using Impacta.Infra.Apoio;
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CSharp2.Capitulo02.Produtos
+{
+    public partial class ProdutoForm : Form
+    {
+        private static readonly log4net.ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ProdutoForm()
+        {
+            InitializeComponent();
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        private void gravarButton_Click(object sender, EventArgs e)
+        {
+            if (!Formulario.ValidarCamposObrigatorios(this, produtoErrorProvider) || !Formulario.ValidarTipoDosDados(this, produtoErrorProvider))
+            {
+                return;
+            }
+
+            try
+            {
+                GravarProduto();
+            }
+            catch (SqlException ex)
+            {
+                if (ex.Message.Contains("UK_Produto_Descricao"))
+                {
+                    MessageBox.Show("Já existe um produto cadastrado com esta descrição e tipo.");
+                }
+                else
+                {
+                    TratarErro(ex);
+                }
+            }
+            catch (Exception ex)
+            {
+                TratarErro(ex);
+            }
+        }
+
+        private void TratarErro(Exception ex)
+        {
+            MessageBox.Show("Houve um erro e a gravação não foi realizada! O suporte já foi comunicado.");
+            //Logar.PorEmail(ex);
+            _log.Error(ex);
+        }
+
+        private void GravarProduto()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
