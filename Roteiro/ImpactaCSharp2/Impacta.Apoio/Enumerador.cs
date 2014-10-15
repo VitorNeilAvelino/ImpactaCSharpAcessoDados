e3847474880511eca704f93e8354773ba2fdbf6a
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2014-10-15 02:06:27 +0000

    Enumerador.ParaLista<>().

---
+++ Roteiro/ImpactaCSharp2/Impacta.Apoio/Enumerador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Impacta.Apoio
+{
+    public static class Enumerador
+    {
+        public static List<T> ParaLista<T>()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>().OrderBy(x => x.ToString()).ToList();
+        }
+    }
+}
