7afcabd8bbb365228c733daf6a3e7e1dd2802687
vitoravelino_cp <SND\vitoravelino_cp>
2014-10-28 01:28:49 +0000

    Edição de produto.

--- Roteiro/ImpactaCSharp2/Impacta.Infra.Repositorios.SqlServer/ProdutoRepositorio.cs
+++ Roteiro/ImpactaCSharp2/Impacta.Infra.Repositorios.SqlServer/ProdutoRepositorio.cs
@@ -1,6 +1,8 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
+using Impacta.Dominio;
+using System;
 
 namespace Impacta.Repositorios.SqlServer.Proc
 {
@@ -41,7 +43,59 @@ namespace Impacta.Repositorios.SqlServer.Proc
 
         public void Excluir(int produtoId)
         {
-            throw new System.NotImplementedException();
+            using (var conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["PedidosConnectionString"].ConnectionString))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.CommandText = "ExcluirProduto";
+                    comando.Parameters.AddWithValue("@produtoId", produtoId);
+                    comando.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public Produto Selecionar(int produtoId)
+        {
+            Produto produto = null;
+
+            using (var conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["PedidosConnectionString"].ConnectionString))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.CommandText = "SelecionarProduto";
+                    comando.Parameters.AddWithValue("@produtoId", produtoId);
+
+                    using (var registro = comando.ExecuteReader())
+                    {
+                        if (registro.Read())
+                        {
+                            produto = Mapear(registro);
+                        }
+                    }
+                }
+            }
+
+            return produto;
+        }
+
+        private Produto Mapear(SqlDataReader registro)
+        {
+            var produto = new Produto();
+
+            produto.Id = Convert.ToInt32(registro["Id"]);
+            produto.Descricao = registro["Descricao"].ToString();
+            produto.Custo = Convert.ToDecimal(registro["Custo"]);
+
+            produto.Tipo.Id = Convert.ToInt32(registro["IdTipoProduto"]);
+            produto.Tipo.Descricao = registro["DescricaoTipoProduto"].ToString();
+
+            return produto;
         }
     }
 }
