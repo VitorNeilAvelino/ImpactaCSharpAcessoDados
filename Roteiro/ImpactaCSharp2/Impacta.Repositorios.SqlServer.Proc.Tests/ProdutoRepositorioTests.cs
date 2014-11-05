e6a54a7dba66de16ccc54438e15e0b14edbfe55f
vitoravelino_cp <SND\vitoravelino_cp>
2014-11-05 01:00:17 +0000

    Nível de isolamento.

---
+++ Roteiro/ImpactaCSharp2/Impacta.Repositorios.SqlServer.Proc.Tests/ProdutoRepositorioTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Impacta.Repositorios.SqlServer.Proc.Tests
+{
+    [TestClass()]
+    public class ProdutoRepositorioTests
+    {
+        [TestMethod()]
+        public void AtualizarComTransacaoTest()
+        {
+            var repositorio = new ProdutoRepositorio();
+
+            repositorio.AtualizarComTransacao();
+
+            var produto = repositorio.Selecionar(3);
+
+            Assert.AreEqual(produto.Descricao, "Caneta - transação");
+            Assert.AreNotEqual(produto.Descricao, "Caneta");
+        }
+    }
+}
