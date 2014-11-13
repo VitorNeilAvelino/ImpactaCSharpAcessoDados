cc8b8b365c32ad53828b1e5f44894c602dcd3faa
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2014-11-13 20:15:40 +0000

    WPF.

---
+++ Roteiro/ImpactaCSharp2/CSharp2.Capitulo10.Wpf.Produtos/PedidosWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Impacta.Repositorios.Ef.Designer;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace CSharp2.Capitulo10.Wpf.Produtos
+{
+    public partial class PedidosWindow : Window, INotifyPropertyChanged
+    {
+        public PedidosWindow()
+        {
+            InitializeComponent();
+            PopularPropriedades();
+        }
+
+        private void PopularPropriedades()
+        {
+            using (var contexto = new PedidosEntities())
+            {
+                Vendedores = new ObservableCollection<Vendedor>(contexto.Vendedor.ToList());
+                PropertyChanged(this, new PropertyChangedEventArgs("Vendedores"));
+            }
+        }
+
+        public ObservableCollection<Vendedor> Vendedores { get; set; }
+        public Cliente Cliente { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
