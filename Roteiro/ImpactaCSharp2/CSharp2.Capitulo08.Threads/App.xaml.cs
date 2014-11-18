898424a379c80d6892e22e148436cdd1e18ed1a7
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2014-11-18 22:30:32 +0000

    Thread.

---
+++ Roteiro/ImpactaCSharp2/CSharp2.Capitulo08.Threads/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Markup;
+
+namespace CSharp2.Capitulo08.Threads
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(XmlLanguage.GetLanguage("pt-BR")));
+        }
+    }
+}
