97877f3f76b8c984cc0d5ca63e4d95faa4266731
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2014-11-25 00:42:21 +0000

    WPF.

---
+++ Roteiro/ImpactaCSharp2/CSharp2.Capitulo10.Wpf.Produtos/Converters/MultiplicadorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace CSharp2.Capitulo10.Wpf.Produtos.Converters
+{
+    public class MultiplicadorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                return (System.Convert.ToDecimal(values[0]) * System.Convert.ToDecimal(values[1])).ToString();
+            }
+            catch 
+            {
+                return null;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException("Not implemented");
+        }
+    }
+}
--- Roteiro/ImpactaCSharp2/CSharp2.Capitulo10.Wpf.Produtos/PedidosWindow.xaml.cs
+++ Roteiro/ImpactaCSharp2/CSharp2.Capitulo10.Wpf.Produtos/PedidosWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using Impacta.Dominio;
 using System;
 using System.Windows.Media;
+using Produto = Impacta.Repositorios.Ef.Designer.Produto;
 
 namespace CSharp2.Capitulo10.Wpf.Produtos
 {
@@ -18,22 +19,37 @@ namespace CSharp2.Capitulo10.Wpf.Produtos
             PopularPropriedades();
         }
 
+        #region ViewModel
         public List<Impacta.Repositorios.Ef.Designer.Vendedor> Vendedores { get; set; }
-        public Impacta.Repositorios.Ef.Designer.Cliente Cliente { get; set; }
         public List<Impacta.Repositorios.Ef.Designer.Produto> Produtos { get; set; }
+        public Impacta.Repositorios.Ef.Designer.Pedido Pedido { get; set; }
+        public decimal TotalPedido
+        {
+            get
+            {
+                if (Pedido == null)
+                {
+                    return 0;
+                }
 
-        //PedidosEntities _contexto = new PedidosEntities();
+                return Pedido.ItensPedido.Sum(i => (i.PrecoUnitario * i.Quantidade));
+            }
+        }
+        #endregion
+
+        PedidosEntities _contexto = new PedidosEntities();
 
         private void PopularPropriedades()
         {
-            using (var _contexto = new PedidosEntities())
-            {
+            //using (var _contexto = new PedidosEntities())
+            //{
                 Vendedores = _contexto.Vendedor.Include("Pessoa").ToList();
                 Produtos = _contexto.Produto.Include("TipoProduto").ToList();
+                Pedido = new Impacta.Repositorios.Ef.Designer.Pedido();
 
                 PropertyChanged(this, new PropertyChangedEventArgs("Vendedores"));
                 PropertyChanged(this, new PropertyChangedEventArgs("Produtos"));
-            }
+            //}
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -48,14 +64,14 @@ namespace CSharp2.Capitulo10.Wpf.Produtos
 
         private void SelecionarClientePorCpf()
         {
-            using (var _contexto = new PedidosEntities())
-            {
-                Cliente = _contexto.Cliente.Include("Pessoa")
+            //using (var _contexto = new PedidosEntities())
+            //{
+                Pedido.Cliente = _contexto.Cliente.Include("Pessoa")
                     .SingleOrDefault(
                         c => c.Pessoa.PessoaDocumentos.Any(
                             d => d.Tipo == (int)TipoDocumento.Cpf && d.Numero == cpfCliente.Text));
 
-                if (Cliente == null)
+                if (Pedido.Cliente == null)
                 {
                     nomeCliente.Content = "CPF não encontrado";
                     cpfCliente.BorderBrush = Brushes.Red;
@@ -63,14 +79,72 @@ namespace CSharp2.Capitulo10.Wpf.Produtos
                 }
 
                 cpfCliente.BorderBrush = Brushes.Black;
-                nomeCliente.Content = Cliente.Pessoa.Nome;
-            }
+                nomeCliente.Content = Pedido.Cliente.Pessoa.Nome;
+            //}
         }
 
-        //protected override void OnClosing(CancelEventArgs e)
-        //{
-        //    //_contexto.Dispose();
-        //    base.OnClosing(e);
-        //}
+        private void adicionarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var item = new Impacta.Repositorios.Ef.Designer.ItensPedido();
+
+            item.Quantidade = Convert.ToInt32(quantidadeTextBox.Text);
+            item.Produto = (Produto)produtosComboBox.SelectedItem;
+            item.PrecoUnitario = item.Produto.Custo;
+
+            Pedido.ItensPedido.Add(item);
+            PropertyChanged(this, new PropertyChangedEventArgs("Pedido"));
+            PropertyChanged(this, new PropertyChangedEventArgs("TotalPedido"));
+            itensPedidoDataGrid.Items.Refresh();
+            quantidadeTextBox.Text = "1";
+        }
+
+        private void removerButton_Click(object sender, RoutedEventArgs e)
+        {
+            var item = ((FrameworkElement)sender).DataContext as ItensPedido;
+
+            Pedido.ItensPedido.Remove(item);
+            PropertyChanged(this, new PropertyChangedEventArgs("Pedido"));
+            PropertyChanged(this, new PropertyChangedEventArgs("TotalPedido"));
+            itensPedidoDataGrid.Items.Refresh();
+        }
+
+        private void itensPedidoDataGrid_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            itensPedidoScrollViewer.ScrollToVerticalOffset(itensPedidoScrollViewer.VerticalOffset - e.Delta);
+        }
+
+        private void fecharPedidoButton_Click(object sender, RoutedEventArgs e)
+        {
+            //using (var contexto = new PedidosEntities())
+            //{
+                Pedido.Vendedor = (Impacta.Repositorios.Ef.Designer.Vendedor)vendedorComboBox.SelectedItem;
+                Pedido.DataEmissao = DateTime.Now;
+
+                _contexto.Pedido.Add(Pedido);
+
+                _contexto.SaveChanges();
+
+                MessageBox.Show("Pedido realizado com sucesso.");
+
+                LimparFormulario();
+            //}
+        }
+
+        private void LimparFormulario()
+        {
+            cpfCliente.Text = string.Empty;
+            nomeCliente.Content = string.Empty;
+            produtosComboBox.SelectedIndex = -1;
+            
+            Pedido = new Pedido();
+            PropertyChanged(this, new PropertyChangedEventArgs("Pedido"));
+            PropertyChanged(this, new PropertyChangedEventArgs("TotalPedido"));
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            _contexto.Dispose();
+            base.OnClosing(e);
+        }
     }
 }
